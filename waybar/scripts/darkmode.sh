8c5d0a55205e750f2cb834418652e98cd94083c5
Flexksx <cristian.cretu2808@gmail.com>
2024-07-23 17:13:35 +0300

    add rofi config

--- waybar/scripts/darkmode.sh
+++ waybar/scripts/darkmode.sh
@@ -3,7 +3,7 @@ DIR='/home/flexksx/dotfiles-Hyprland/backrgrounds/dark'
 BG=$(find "$DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" \) | shuf -n 1)
 swww img $BG --transition-duration 1.2 --transition-step 255 --transition-type outer --transition-pos 0,0
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-wal -i $BG --saturate 0.4
+wal -i $BG --saturate 0.4 --iterative --cols16 darken
 
 STYLE_PATH='/home/flexksx/dotfiles-Hyprland/waybar/style.css'
 content=$(cat "$STYLE_PATH")
--- waybar/scripts/lightmode.sh
+++ waybar/scripts/lightmode.sh
@@ -4,7 +4,7 @@ DIR='/home/flexksx/dotfiles-Hyprland/backrgrounds/light'
 BG=$(find "$DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.gif" \) | shuf -n 1)
 swww img $BG --transition-duration 1.2 --transition-step 255 --transition-type outer --transition-pos 0,0 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-wal -i $BG -l --saturate 0.4 
+wal -i $BG -l --saturate 0.4 --iterative --cols16 lighten
 
 STYLE_PATH='/home/flexksx/dotfiles-Hyprland/waybar/style.css'
 content=$(cat "$STYLE_PATH")
